057860515b166090faa22a44aec294bc9fc6b19a
tonyisaway <tonyisaway@hotmail.com>
2016-12-18 20:29:23 +0000

    Web, Core, Infrastructure

---
+++ Solution/TimeSheets.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeSheets.Web.Startup))]
+namespace TimeSheets.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
